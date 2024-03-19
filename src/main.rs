1991ffdd19459a358ba75f50a087c02ef0476395
RGGH <pisscompany@gmail.com>
2024-03-19 16:22:19 +0000

    autosport

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,18 @@
-use serde::{Deserialize, Serialize};
-use surrealdb::engine::local::RocksDb;
+#![allow(unused)]
+use colored::Colorize;
+use serde::Deserialize;
 use surrealdb::engine::local::Db;
-
+use surrealdb::engine::local::RocksDb;
 use surrealdb::sql::Thing;
 use surrealdb::Surreal;
 
 #[derive(Debug, Deserialize)]
-struct Entry {
+struct Magazine {
     name: String,
-    phone: String,
+    price: f32,
+    day: u8,
+    month: u8,
+    year: u32,
 }
 
 #[tokio::main]
@@ -17,43 +21,63 @@ async fn main() -> surrealdb::Result<()> {
     let mut db_path = std::env::current_dir().unwrap();
     db_path.push("db");
     println!("{db_path:?}");
+
+    // use embedded RocksDB for storage
     let db = Surreal::new::<RocksDb>(db_path).await?;
 
-    // Select a specific namespace / database
+    // Select specific namespace & database
     db.use_ns("test").use_db("test").await?;
 
     let _response = db
-        .query("DEFINE INDEX entry_email ON TABLE entry COLUMNS name UNIQUE")
+        //-- Create an index on the name, month and year fields of the product table
+        // DEFINE INDEX test ON user FIELDS account, email;
+        .query("DEFINE INDEX magid ON TABLE product COLUMNS name,month,year UNIQUE")
         .await?;
 
-    let data = vec![("Joe", "123"), ("Jane", "456"), ("Jil", "789")];
+    let data = vec![
+        Magazine {
+            name: "Autosport".to_string(),
+            price: 1.80,
+            day: 12,
+            month: 12,
+            year: 1987,
+        },
+        Magazine {
+            name: "Autosport".to_string(),
+            price: 2.10,
+            day: 1,
+            month: 9,
+            year: 1985,
+        },
+        Magazine {
+            name: "Autosport".to_string(),
+            price: 0.90,
+            day: 22,
+            month: 1,
+            year: 1984,
+        },
+    ];
 
     add_to(&db, data).await?;
     list_all(&db).await?;
-    println!("-------------");
+    println!("--------------------------------------------");
 
-    //update(&db).await?;
-    //list_all(&db).await?;
-    //println!("-------------");
-
-    //delete(&db).await?;
-    //list_all(&db).await?;
-    //println!("-------------");
-
-    //add_to(&db, vec![("Joe", "7777777")]).await?;
-    //println!("this does not happen");
-    //list_all(&db).await?;
-    //println!("-------------");
+    list_star(&db).await?;
+    println!("--------------------------------------------");
 
     Ok(())
 }
 
-async fn add_to(db: &Surreal<Db>, data: Vec<(&str, &str)>) -> surrealdb::Result<()> {
-    for (name, phone) in data {
+async fn add_to(db: &Surreal<Db>, data: Vec<Magazine>) -> surrealdb::Result<()> {
+    for magazine in data {
         let response = db
-            .query("CREATE entry SET  name=$name, phone=$phone")
-            .bind(("name", name))
-            .bind(("phone", phone))
+            .query("CREATE product SET  name=$name, 
+                   price=$price, day=$day, month=$month, year=$year")
+            .bind(("name", magazine.name))
+            .bind(("price", magazine.price))
+            .bind(("day", magazine.day))
+            .bind(("month", magazine.month))
+            .bind(("year", magazine.year))
             .await?;
 
         match response.check() {
@@ -69,51 +93,38 @@ async fn add_to(db: &Surreal<Db>, data: Vec<(&str, &str)>) -> surrealdb::Result<
 
 async fn list_all(db: &Surreal<Db>) -> surrealdb::Result<()> {
     let mut entries = db
-        .query("SELECT name, phone FROM type::table($table) ORDER BY name ASC")
-        //.query("SELECT name, phone FROM type::table($table) where name='Jil'")
-        .bind(("table", "entry"))
+        .query("SELECT name, price, day, month, year 
+               FROM type::table($table) ORDER BY name ASC")
+        .bind(("table", "product"))
         .await?;
-    let entries: Vec<Entry> = entries.take(0)?;
+    let entries: Vec<Magazine> = entries.take(0)?;
+    println!(
+        "{:<12} {:5} {:<2} {:<2} {:<2}",
+        "Magazine", "price", "day", "month", "year"
+    );
+    println!("--------------------------------------------");
     for entry in entries {
-        println!("{}: {}", entry.name, entry.phone);
+        println!(
+            "{:<12} {:<5.2} {:<3} {:<5} {:<4}",
+            entry.name.yellow(),
+            entry.price,
+            entry.day,
+            entry.month,
+            entry.year
+        );
     }
 
     Ok(())
 }
 
-async fn update(db: &Surreal<Db>) -> surrealdb::Result<()> {
-    let name = "Jane";
-    let phone = "55555555";
-
-    let response = db
-        .query("UPDATE entry SET phone=$phone WHERE name=$name")
-        .bind(("name", name))
-        .bind(("phone", phone))
+async fn list_star(db: &Surreal<Db>) -> surrealdb::Result<()> {
+    let mut entries = db
+        .query("SELECT * FROM type::table($table)")
+        .bind(("table", "product"))
         .await?;
-
-    match response.check() {
-        Ok(_) => Ok(()),
-        Err(err) => {
-            eprintln!("Could not add entry {}", err);
-            Err(err)
-        }
+    let entries: Vec<Magazine> = entries.take(0)?;
+    for entry in entries {
+        println!("{:?} ", entry);
     }
-}
-
-
-async fn delete(db: &Surreal<Db>) -> surrealdb::Result<()> {
-    let name = "Jane";
-
-    let response = db
-        .query("DELETE entry WHERE name=$name")
-        .bind(("name", name))
-        .await?;
-
-    match response.check() {
-        Ok(_) => Ok(()),
-        Err(err) => {
-            eprintln!("Could not delete entry {}", err);
-            Err(err)
-        }
-    }
+    Ok(())
 }
