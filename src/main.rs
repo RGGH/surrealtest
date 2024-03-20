6a48f747ca55a39a238771db0ef7bfa5701271a2
RGGH <pisscompany@gmail.com>
2024-03-20 22:18:35 +0000

    take user input

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,25 @@
+// run as ./target/debug/surrealtest 13 2 1998
 #![allow(unused)]
+use std::fmt::format;
+
+use clap::Parser;
 use colored::Colorize;
 use serde::Deserialize;
 use surrealdb::engine::local::Db;
 use surrealdb::engine::local::RocksDb;
 use surrealdb::sql::Thing;
 use surrealdb::Surreal;
+//use surrealkv::Store;
+
+#[derive(Parser, Debug)]
+struct Value {
+    /// day number to add
+    number_day: i32,
+    /// month number to add
+    number_month: i32,
+    /// year number to add
+    number_year: i32,
+}
 
 #[derive(Debug, Deserialize)]
 struct Magazine {
@@ -27,13 +42,24 @@ struct Record {
 
 #[tokio::main]
 async fn main() -> surrealdb::Result<()> {
+    let value = Value::parse();
+
+    let day = value.number_day;
+    let month = value.number_month;
+    let year = value.number_year;
+
+    let next = format!("Day:{} Month:{} Year:{}",day,month,year);
+
+    println!("{}", next.green());
+
     // Create database connection
     let mut db_path = std::env::current_dir().unwrap();
     db_path.push("db");
-    println!("{db_path:?}");
+    //println!("{db_path:?}");
 
-    // use embedded RocksDB for storage
+    // use embedded RocksDB for storage -try SurrealKV as soon as it compiles ok
     let db = Surreal::new::<RocksDb>(db_path).await?;
+    //let db = Surreal::new::<Store>(db_path).await?;
 
     // Select specific namespace & database
     db.use_ns("test").use_db("test").await?;
@@ -68,14 +94,33 @@ async fn main() -> surrealdb::Result<()> {
             month: 1,
             year: 1984,
         },
+        Magazine {
+            name: "Autosport".to_string(),
+            price: 1.20,
+            day: 23,
+            month: 9,
+            year: 1984,
+        },
     ];
+
+    println!(
+        r"
+         __                       ____  
+   _____/ /_____  ____  ___  ____/ / /_ 
+  / ___/ __/ __ \/ __ \/ _ \/ __  / __ \
+ (__  ) /_/ /_/ / / / /  __/ /_/ / /_/ /
+/____/\__/\____/_/ /_/\___/\__,_/_.___/ "
+    );
 
     add_to(&db, data).await?;
     list_all(&db).await?;
-    println!("--------------------------------------------");
+    println!("----------------------------------------------------------------");
 
     list_year(&db, 1987).await?;
-    println!("--------------------------------------------");
+    println!("----------------------------------------------------------------");
+
+    //add_relate(&db,"Senna".to_string());
+    list_related(&db);
 
     Ok(())
 }
@@ -108,25 +153,27 @@ async fn add_to(db: &Surreal<Db>, data: Vec<Magazine>) -> surrealdb::Result<()> 
 async fn list_all(db: &Surreal<Db>) -> surrealdb::Result<()> {
     let mut entries = db
         .query(
-            "SELECT name, price, day, month, year 
+            "SELECT name, price, day, month, year, id 
                FROM type::table($table) ORDER BY name ASC",
         )
         .bind(("table", "product"))
         .await?;
-    let entries: Vec<Magazine> = entries.take(0)?;
+    let entries: Vec<Record> = entries.take(0)?;
+    println!("----------------------------------------------------------------");
     println!(
-        "{:<12} {:5} {:<2} {:<2} {:<2}",
-        "Magazine", "price", "day", "month", "year"
+        "{:<12} {:5} {:<2} {:<2} {:<2} {:}",
+        "Magazine", "price", "day", "month", "year", "table+id"
     );
-    println!("--------------------------------------------");
+    println!("----------------------------------------------------------------");
     for entry in entries {
         println!(
-            "{:<12} {:<5.2} {:<3} {:<5} {:<4}",
+            "{:<12} {:<5.2} {:<3} {:<5} {:<4} {:}",
             entry.name.yellow(),
             entry.price,
             entry.day,
             entry.month,
-            entry.year
+            entry.year,
+            entry.id.to_raw().blue()
         );
     }
 
@@ -140,6 +187,26 @@ async fn list_year(db: &Surreal<Db>, year: u32) -> surrealdb::Result<()> {
         .bind(("year", year))
         .await?;
     let entries: Vec<Magazine> = entries.take(0)?;
+    for entry in entries {
+        println!("{:?} ", entry);
+    }
+    Ok(())
+}
+
+async fn add_relate(db: &Surreal<Db>, topic: String) -> surrealdb::Result<()> {
+    let _relate = db
+        .query("RELATE product->featured->($topic)")
+        .bind(("topic", topic))
+        .await?;
+    Ok(())
+}
+
+async fn list_related(db: &Surreal<Db>) -> surrealdb::Result<()> {
+    let mut entries = db
+        .query("SELECT * FROM type::table($table)")
+        .bind(("table", "featured"))
+        .await?;
+    let entries: Vec<Record> = entries.take(0)?;
     for entry in entries {
         println!("{:?} ", entry);
     }
